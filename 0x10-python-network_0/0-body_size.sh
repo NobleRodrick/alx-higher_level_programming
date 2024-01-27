fbd6cf2fb25e17fadeda3c83ebb616d88f5649d1
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-01-27 14:56:10 +0100

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response.
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+curl -s "$1" | wc -c
